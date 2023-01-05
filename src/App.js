7a71ba5311808edf553da4387d994530f085069f
G-Dharani-raj <105777295+G-Dharani-raj@users.noreply.github.com>
2023-01-05 21:55:07 +0530

    some

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import TodoApp from "./components/TodoApp";
 
 function App() {
-  return (
-    <div className="App">
-      <TodoApp />
-    </div>
-  );
+	return (
+		<div className="App">
+			<TodoApp />
+		</div>
+	);
 }
 
 export default App;
